86db96ce1f6e72ce69462b62636227f1ed60cd23
SamuelMends <samuelqmendes@gmail.com>
2024-05-21 22:01:31 -0300

    comiit

--- train_forza.py
+++ train_forza.py
@@ -1,6 +1,9 @@
-from ultralytics import YOLO
+from ultralytics import YOLO # type: ignore
 
 def main():
     model = YOLO("yolov8n.pt")
-    model.train(data="mlpath.yaml", epochs=30, device=0)
+    model.train(data="mlpath.yaml", epochs=30)
     model.val()
+
+if __name__== '__main__':
+    main()
