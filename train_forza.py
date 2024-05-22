450f07aebdc7de529fd6f6d22e5fc4fd5a7fb4a3
SamuelMends <samuelqmendes@gmail.com>
2024-05-22 19:08:49 -0300

    Update train_forza.py

--- train_forza.py
+++ train_forza.py
@@ -1,1 +1,5 @@
 ## Construindo sua própria base de imagens
+# Precisamos de imagens dos objetos que queremos reconhecer, já catalogadas (com os arquivos annotation)
+# Arquivos obj.data e obj.names
+# Arquivo .cfg customizado 
+# Arquivo train.txt (test.txt é opcional)
